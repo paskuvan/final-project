21c562f8f1f52998713c3f9ecd3f751a2bc3d10a
paskuvan <majopaskuvan@gmail.com>
2019-04-17 15:45:02 -0300

    fixed

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    end
 
    version :default do
-     process resize_to_fit: [500, 500]
+     process resize_to_fit: [800, 600]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_220107) do
+ActiveRecord::Schema.define(version: 2019_04_17_114258) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "brand"
