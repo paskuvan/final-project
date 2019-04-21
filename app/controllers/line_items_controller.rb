a910a133e2b1366d4c92c8f47c83f6274ae84d75
paskuvan <majopaskuvan@gmail.com>
2019-04-21 00:48:09 -0300

    finish

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,7 +57,7 @@ end
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @cart = Card.find(session[:cart_id])
+    @cart = Cart.find(session[:cart_id])
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to cart_path(@cart), notice: 'Item successfully removed.' }
