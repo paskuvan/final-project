8846dac7f15f735864cbbd2b861519b28cce7f6c
paskuvan <majopaskuvan@gmail.com>
2019-04-20 22:38:16 -0300

    add controller cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    current_item = lines_items.find_by(instrument_id: instrument.id)
+
+    if current_item
+      current_item.increment(:quantity)
+    else
+      current_item = line.items.build(product_id: product.id)
+  end
+  current_item
+end
+
 end
